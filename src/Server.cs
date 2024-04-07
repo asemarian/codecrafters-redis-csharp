31cd91ac75394c7112cb16584efe633d02e971d3
Asem Zletni <asem.zletni@gmail.com>
2024-04-07 01:03:18 +0000

    feat: add while loop to read input and respond statically

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,84 @@
 using System.Net;
 using System.Net.Sockets;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+//         TcpListener server = null;
+//     try
+//     {
+//       // Set the TcpListener on port 13000.
+//       Int32 port = 13000;
+//       IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+//       // TcpListener server = new TcpListener(port);
+//       server = new TcpListener(localAddr, port);
+
+//       // Start listening for client requests.
+//       server.Start();
+
+//       // Buffer for reading data
+//       Byte[] bytes = new Byte[256];
+//       String data = null;
+
+//       // Enter the listening loop.
+//       while(true)
+//       {
+//         Console.Write("Waiting for a connection... ");
+
+//         // Perform a blocking call to accept requests.
+//         // You could also use server.AcceptSocket() here.
+//         using TcpClient client = server.AcceptTcpClient();
+//         Console.WriteLine("Connected!");
+
+//         data = null;
+
+//         // Get a stream object for reading and writing
+//         NetworkStream stream = client.GetStream();
+
+//         int i;
+
+//         // Loop to receive all the data sent by the client.
+//         while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+//         {
+//           // Translate data bytes to a ASCII string.
+//           data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+//           Console.WriteLine("Received: {0}", data);
+
+//           // Process the data sent by the client.
+//           data = data.ToUpper();
+
+//           byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+//           // Send back a response.
+//           stream.Write(msg, 0, msg.Length);
+//           Console.WriteLine("Sent: {0}", data);
+//         }
+//       }
+//     }
+//     catch(SocketException e)
+//     {
+//       Console.WriteLine("SocketException: {0}", e);
+//     }
+//     finally
+//     {
+//       server.Stop();
+//     }
+
+//     Console.WriteLine("\nHit enter to continue...");
+//     Console.Read();
+//   }
+
+        TcpListener server = new TcpListener(IPAddress.Any, 6379);
+        server.Start();
+        server.AcceptSocket();
+
+        while (true)
+        {
+            Console.Write("redis-cli: ");
+            var input = Console.ReadLine();
+            Console.WriteLine("+PONG\r\n");
+        }
+    }
+}
