dadc7e78df0442078e782f93f1d442a881e0e893
Asem Zletni <asem.zletni@gmail.com>
2024-04-19 20:36:57 +0000

    support multiple concurrent clients

--- src/Server.cs
+++ src/Server.cs
@@ -1,65 +1,39 @@
 using System.Net;
 using System.Net.Sockets;
 
-internal class Program
+
+using TcpListener server = new(IPAddress.Any, 6379);
+
+try
 {
-    private static void Main(string[] args)
+    server.Start();
+
+    while (true)
     {
-        TcpListener server = new TcpListener(IPAddress.Any, 6379);
+        var client = await server.AcceptTcpClientAsync();
+        _ = Task.Run(() => HandleClient(client));
+    }
+}
+catch (SocketException e)
+{
+    Console.WriteLine("SocketException: {0}", e);
+}
 
-        try
-        {
-            // Buffer for reading data
-            Byte[] bytes = new Byte[256];
-            String data = null;
+Console.WriteLine("\nHit enter to continue...");
+Console.Read();
 
-            // Enter the listening loop.
-            while (true)
-            {
-                Console.Write("Waiting for a connection... ");
+void HandleClient(TcpClient client)
+{
+    Console.WriteLine("Connected!");
+    NetworkStream stream = client.GetStream();
 
-                // Perform a blocking call to accept requests.
-                // You could also use server.AcceptSocket() here.
-                server.Start();
-                using TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("Connected!");
+    while (client.Connected)
+    {
+        byte[] buffer = new byte[256];
+        stream.Read(buffer);
 
-                // Get a stream object for reading and writing
-                NetworkStream stream = client.GetStream();
+        var command = System.Text.Encoding.ASCII.GetString(buffer);
 
-                int i;
-
-                // Loop to receive all the data sent by the client.
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    // Translate data bytes to a ASCII string.
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
-
-                    // Process the data sent by the client.
-                    data = data.ToUpper();
-
-                    // byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-                    byte[] msg2 = System.Text.Encoding.ASCII.GetBytes("+PONG\r\n");
-
-
-                    // Send back a response.
-                    // stream.Write(msg, 0, msg.Length);
-                    stream.Write(msg2, 0, msg2.Length);
-                    Console.WriteLine("Sent: {0}", "+PONG\r\n");
-                }
-            }
-        }
-        catch (SocketException e)
-        {
-            Console.WriteLine("SocketException: {0}", e);
-        }
-        finally
-        {
-            server.Stop();
-        }
-
-        Console.WriteLine("\nHit enter to continue...");
-        Console.Read();
+        stream.Write(System.Text.Encoding.ASCII.GetBytes("+PONG\r\n"));
     }
 }
